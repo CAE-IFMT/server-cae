a565c5f32bea18c9aff736d1c6bcd15c3df8aeb8
Gabriel Honda <gabrielhondacba@gmail.com>
2021-01-23 16:30:57 -0400

    Initial commit

---
+++ src/main/kotlin/br/edu/ifmt/controledeacesso/ControleDeAcessoApplication.kt
@@ -0,0 +1,11 @@
+package br.edu.ifmt.controledeacesso
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ControleDeAcessoApplication
+
+fun main(args: Array<String>) {
+  runApplication<ControleDeAcessoApplication>(*args)
+}
---
+++ src/test/kotlin/br/edu/ifmt/controledeacesso/ControleDeAcessoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.edu.ifmt.controledeacesso
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ControleDeAcessoApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
