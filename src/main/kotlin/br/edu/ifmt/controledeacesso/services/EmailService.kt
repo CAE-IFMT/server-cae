7e9f86b38c8e2841eb887fb0129252a94c685d29
Gabriel Honda <gabrielhondacba@gmail.com>
2021-01-27 08:45:04 -0400

    wip: cria serviço responsável pela regra de negócio relacionadas ao e-mail

---
+++ src/main/kotlin/br/edu/ifmt/controledeacesso/services/EmailService.kt
@@ -0,0 +1,11 @@
+package br.edu.ifmt.controledeacesso.services
+
+/**
+ * Interface que define regras de negócio relacionadas
+ * ao envio e recebimento de e-mail.
+ *
+ * @project cae-api
+ * @author daohn on 26/01/2021
+ */
+interface EmailService {
+}
