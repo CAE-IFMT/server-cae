b1bfbd1c94f5d92def69ebe4a6d03ae8b8624ddc
Gabriel Honda <gabrielhondacba@gmail.com>
2021-02-06 16:33:48 -0400

    fix: adiciona anotação Repository

--- src/main/kotlin/br/edu/ifmt/controledeacesso/repositories/UsuarioRepository.kt
+++ src/main/kotlin/br/edu/ifmt/controledeacesso/repositories/UsuarioRepository.kt
@@ -2,10 +2,12 @@ package br.edu.ifmt.controledeacesso.repositories
 
 import br.edu.ifmt.controledeacesso.models.entities.Usuario
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
 /**
  * @project cae-api
  * @author daohn on 26/01/2021
  */
+@Repository
 interface UsuarioRepository : JpaRepository<Usuario, Long> {
 }
--- src/main/kotlin/br/edu/ifmt/controledeacesso/repositories/VisitanteRepository.kt
+++ src/main/kotlin/br/edu/ifmt/controledeacesso/repositories/VisitanteRepository.kt
@@ -2,9 +2,11 @@ package br.edu.ifmt.controledeacesso.repositories
 
 import br.edu.ifmt.controledeacesso.models.entities.Visitante
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
 /**
  * @project cae-api
  * @author daohn on 24/01/2021
  */
+@Repository
 interface VisitanteRepository : JpaRepository<Visitante, Long>
