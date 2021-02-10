9c5cf26d42891ed277dc525cb636d90817a7fad1
Gabriel Honda <gabrielhondacba@gmail.com>
2021-02-10 17:05:32 -0400

    test: atualiza testes

--- src/test/kotlin/br/edu/ifmt/controledeacesso/services/VisitaServiceTest.kt
+++ src/test/kotlin/br/edu/ifmt/controledeacesso/services/VisitaServiceTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.time.format.DateTimeFormatter
 
 /**
  * Created by daohn on 10/02/2021
@@ -22,16 +23,20 @@ class VisitaServiceTest {
 
   @Test
   fun saveTest() {
+    val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
     val visitaSave = VisitaSaveDTO(
-      "11/02/2020 15:50",
+      "11/02/2020",
       "motivo aqui",
-      false,
       ProfessorDTO(null, "nome professor", "email professor"),
       VisitanteDTO(null, "nome visitante", "email visitante", "cpf")
     )
     val visita = service.save(visitaSave)
 
+    println(visitaSave)
+    println(visita)
+
     assertEquals(visitaSave.professor.nome, visita.professor.nome)
+    assertEquals(visitaSave.data, formatter.format(visita.data))
     assertEquals(visitaSave.visitante.nome, visita.visitante.nome)
     assertEquals(visitaSave.motivo, visita.motivo)
   }
