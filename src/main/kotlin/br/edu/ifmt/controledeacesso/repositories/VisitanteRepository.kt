48bee594fe9c94c312200c01501e3550390308c0
Gabriel Honda <gabrielhondacba@gmail.com>
2021-02-10 11:52:35 -0400

    feat: adiciona consulta de visitante por nome, email e cpf

--- src/main/kotlin/br/edu/ifmt/controledeacesso/repositories/VisitanteRepository.kt
+++ src/main/kotlin/br/edu/ifmt/controledeacesso/repositories/VisitanteRepository.kt
@@ -3,10 +3,13 @@ package br.edu.ifmt.controledeacesso.repositories
 import br.edu.ifmt.controledeacesso.models.entities.Visitante
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 /**
  * @project cae-api
  * @author daohn on 24/01/2021
  */
 @Repository
-interface VisitanteRepository : JpaRepository<Visitante, Long>
+interface VisitanteRepository : JpaRepository<Visitante, Long> {
+  fun findByNomeAndEmailAndCpf(nome: String, email: String, cpf: String): Optional<Visitante>
+}
