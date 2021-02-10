a12431e2b92fd3ebec2ab85c2c0ad8c795974f94
Gabriel Honda <gabrielhondacba@gmail.com>
2021-02-10 10:45:58 -0400

    test: adiciona teste unitário para EmailParserService

---
+++ src/test/kotlin/br/edu/ifmt/controledeacesso/services/EmailParserServiceTest.kt
@@ -0,0 +1,53 @@
+package br.edu.ifmt.controledeacesso.services
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+/**
+ * Created by daohn on 10/02/2021
+ * @author daohn
+ * @since 10/02/2021
+ * @version 1.0.0
+ */
+@SpringBootTest
+class EmailParserServiceTest{
+
+  @Autowired
+  lateinit var parserService: EmailParserService
+
+  @Test
+  fun parserBodyTest() {
+
+    val body = """
+      professor=Gabriel José Curvo Honda
+      visitante=Carlos Henrique
+      email_visitante=carlos@email.com
+      cpf=000.000.000-10
+      data=20/01/2021
+      hora=15:30
+      motivo=descreva aqui, de maneira sucinta, o motivo da visita
+      
+    """.trimIndent()
+
+    val map = parserService.parseBody(body)
+
+    assertEquals("Gabriel José Curvo Honda", map["professor"])
+    assertEquals("Carlos Henrique", map["visitante"])
+    assertEquals("carlos@email.com", map["email_visitante"])
+    assertEquals("000.000.000-10", map["cpf"])
+    assertEquals("descreva aqui, de maneira sucinta, o motivo da visita", map["motivo"])
+  }
+
+  @Test
+  fun parserDateTest() {
+    val map = mutableMapOf<String, String>()
+    map["data"] = "20/01/2021"
+    map["hora"] = "15:30"
+
+    val response = parserService.parseDate(map)
+
+    assertEquals("20/01/2021 15:30:00", response)
+  }
+}
