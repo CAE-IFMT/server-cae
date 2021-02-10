a308592c1e699d17de13578061542516a06867f6
Gabriel Honda <gabrielhondacba@gmail.com>
2021-02-10 11:52:11 -0400

    feat: adiciona consulta de professor por email e nome

--- src/main/kotlin/br/edu/ifmt/controledeacesso/repositories/ProfessorRepository.kt
+++ src/main/kotlin/br/edu/ifmt/controledeacesso/repositories/ProfessorRepository.kt
@@ -3,10 +3,13 @@ package br.edu.ifmt.controledeacesso.repositories
 import br.edu.ifmt.controledeacesso.models.entities.Professor
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 /**
  * @project cae-api
  * @author daohn on 24/01/2021
  */
 @Repository
-interface ProfessorRepository : JpaRepository<Professor, Long>
+interface ProfessorRepository : JpaRepository<Professor, Long> {
+  fun findByEmailAndNome(nome: String, email: String): Optional<Professor>
+}
