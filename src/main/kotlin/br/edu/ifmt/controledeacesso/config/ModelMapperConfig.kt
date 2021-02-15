f260f2614b965d68cbd3807242f3b8f06b42b025
Gabriel Honda <gabrielhondacba@gmail.com>
2021-02-15 08:32:57 -0400

    fix: adiciona formatação de Hora e Minuto

--- src/main/kotlin/br/edu/ifmt/controledeacesso/config/ModelMapperConfig.kt
+++ src/main/kotlin/br/edu/ifmt/controledeacesso/config/ModelMapperConfig.kt
@@ -18,7 +18,7 @@ import java.time.format.DateTimeParseException
 @Configuration
 class ModelMapperConfig {
 
-  private var diaMesAnoHoraMinFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+  private var diaMesAnoHoraMinFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")
 
 
   @Bean
