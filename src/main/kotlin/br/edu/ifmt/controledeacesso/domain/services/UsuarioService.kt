452167095fac93d43ac1530be0aa914648323d0b
Gabriel Honda <gabrielhondacba@gmail.com>
2021-02-19 11:30:20 -0400

    feat: adiciona serviço que implementa UserDetailsService
    Signed-off-by: Gabriel Honda <gabrielhondacba@gmail.com>

---
+++ src/main/kotlin/br/edu/ifmt/controledeacesso/domain/services/UsuarioService.kt
@@ -0,0 +1,22 @@
+package br.edu.ifmt.controledeacesso.domain.services
+
+import br.edu.ifmt.controledeacesso.domain.repositories.UsuarioRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+/**
+ * Created by daohn on 19/02/2021
+ * @author daohn
+ * @since 19/02/2021
+ * @version 1.0.0
+ */
+@Service("UserDetailsService")
+class UsuarioService(val repository: UsuarioRepository) : UserDetailsService {
+
+  @Throws(UsernameNotFoundException::class)
+  override fun loadUserByUsername(login: String): UserDetails = repository
+    .findByLogin(login)
+    .orElseThrow { UsernameNotFoundException("Usuário não encontrado") }
+}
