de7e421acbde74a8d1e3886bbfa8f5771e0f7c1f
Carlos Henrique <carlosheng6@gmail.com>
2021-03-05 21:00:52 -0400

    feat: adiciona buscar por id do visitante

--- src/main/kotlin/br/edu/ifmt/controledeacesso/api/controllers/VisitanteController.kt
+++ src/main/kotlin/br/edu/ifmt/controledeacesso/api/controllers/VisitanteController.kt
@@ -1,10 +1,12 @@
 package br.edu.ifmt.controledeacesso.api.controllers
 
+import br.edu.ifmt.controledeacesso.domain.dto.ProfessorDTO
 import br.edu.ifmt.controledeacesso.domain.dto.VisitanteDTO
 import br.edu.ifmt.controledeacesso.domain.services.VisitanteService
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.annotation.Secured
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,8 +16,14 @@ class VisitanteController(private val service: VisitanteService) {
   @GetMapping
   @Secured("ROLE_ADMIN")
   fun findAll(): ResponseEntity<List<VisitanteDTO>> {
-    val professor = service.findAll()
-    return ResponseEntity.ok(professor)
+    val visitante = service.findAll()
+    return ResponseEntity.ok(visitante)
   }
 
+  @GetMapping("/{id}")
+  @Secured("ROLE_ADMIN")
+  fun findById(@PathVariable("id") id: Long): ResponseEntity<VisitanteDTO> {
+    val visitante = service.findById(id)
+    return ResponseEntity.ok(visitante)
+  }
 }
--- src/main/kotlin/br/edu/ifmt/controledeacesso/domain/services/VisitanteService.kt
+++ src/main/kotlin/br/edu/ifmt/controledeacesso/domain/services/VisitanteService.kt
@@ -1,8 +1,8 @@
 package br.edu.ifmt.controledeacesso.domain.services
-
 import br.edu.ifmt.controledeacesso.domain.dto.VisitanteDTO
 import br.edu.ifmt.controledeacesso.domain.entities.Visitante
 import br.edu.ifmt.controledeacesso.domain.repositories.VisitanteRepository
+import br.edu.ifmt.controledeacesso.exceptions.ObjectNotFoundException
 import org.modelmapper.ModelMapper
 import org.springframework.stereotype.Service
 
@@ -18,4 +18,10 @@ class VisitanteService(
   private fun buildDTO(visitante: Visitante): VisitanteDTO {
     return modelMapper.map(visitante, VisitanteDTO::class.java)
   }
+
+  fun findById(id: Long): VisitanteDTO = repository
+      .findById(id)
+      .map { buildDTO(it) }
+      .orElseThrow { ObjectNotFoundException("Visitante não encontrado") }
+
 }
